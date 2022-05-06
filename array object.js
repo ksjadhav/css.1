cbece97f5807909e40fe69d1b98b40358d031695
ksjadhav <100179580+ksjadhav@users.noreply.github.com>
2022-05-06 21:08:15 +0530

    Create array object.js

---
+++ array object.js
@@ -0,0 +1,22 @@
+ 
+ ​<​html​> 
+ ​<​body​> 
+ ​<​script​ ​type​="​text/javascript​"​> 
+  
+ ​var​ ​city​=​new​ ​Array​(​5​) 
+ ​city​[​0​]​=​"Mumbai" 
+ ​city​[​1​]​=​"Pune" 
+ ​city​[​2​]​=​"Nashik" 
+ ​city​[​3​]​=​"Dhule" 
+ ​city​[​4​]​=​"Shirpur" 
+  
+ ​for​(​i​=​0​;​ ​i​<​5​;​ ​i​++​) 
+ ​{ 
+ ​        ​document​.​write​(​city​[​i​]​+​"<br>"​) 
+ ​} 
+ ​alert​(​city​.​sort​(​)​) 
+ ​alert​(​Math​.​max​(​10​,​20​)​) 
+  
+ ​</​script​> 
+ ​</​body​> 
+ ​</​html​>
